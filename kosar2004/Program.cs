e1c115456189015138df2f8d95569fbd45935fe9
ViczianGabor <viczian.gabor13@gmail.com>
2020-10-15 09:20:02 +0200

    Meccs osztály elkészítése

--- kosar2004/Program.cs
+++ kosar2004/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace kosar2004
 {
@@ -10,8 +11,11 @@ namespace kosar2004
     {
         static void Main(string[] args)
         {
+            //7up Joventut;Adecco Estudiantes;81;73;Palacio Mun. De Deportes De Badalona;2005-04-03
 
 
+            var m = new Meccs("7up Joventut","Adecco Estudiantes",81,73,"Palacio Mun. De Deportes De Badalona", "2005-04-03");
+            Console.WriteLine($"{m.Hazai} - {m.Idegen} - {m.HPont} - {m.IPont}");
             Console.ReadKey();
         }
     }
